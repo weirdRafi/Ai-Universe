68f70c53ac4b9928e99ae702e696825efccfb104
weirdRafi <mazharulrafi@gmail.com>
2023-03-02 21:54:25 +0600

    date added

--- main.js
+++ main.js
@@ -14,10 +14,20 @@ const showAi = data =>{
         <div class="card" style="width: 16rem;">
             <img src="${element.image}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">${element.name}</h5>
+                <h5 class="card-title">Features</h5>
                 <p class="card-text">1.${element.features[0]}</p>
                 <p class="card-text">2.${element.features[1]}</p>
                 <p class="card-text">3.${element.features[2]}</p>
+                <hr>
+                <div class="d-flex justify-content-between">
+                <div>
+                <h4 class="card-text">${element.name}</h4>
+                <p class="card-text"><i class="fa-solid fa-calendar-days"></i> ${element.published_in}</p>
+                </div>
+                <div>
+                <i class="fa-solid fa-arrow-right-long"></i>
+                </div>
+                </div>
                 
             </div>
         </div>
